6dab0353570fef7f79cfa2950c2f425fedfa7771
jtigs <89805712+jtigs@users.noreply.github.com>
2022-04-29 12:59:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ app.use(function(err, req, res, next) {
 //  Starting up the server!
 // *********************************************************** //
 //Here we set the port to use between 1024 and 65535  (2^16-1)
-const port = "4999";
+const port = 4999;
 app.set("port", port);
 
 // and now we startup the server listening on that port
